12d139b611747ee682878a688aaf516f7343dea2
ethanhabedank <120491698+ethanhabedank@users.noreply.github.com>
2023-01-25 16:12:40 -0600

    skate button

---
+++ main.js
@@ -0,0 +1,30 @@
+let btn = document.getElementById('btn')
+let output = document.getElementById('output')
+let quotes = [
+'ollie north',
+'bs pop shuv',
+'fs pop shuv',
+'kickflip',
+'bs 180',
+'fs 180',
+'sw ollie',
+'sw bs pop shuv',
+'sw fs pop shuv',
+'sw fs 180',
+'sw bs 180',
+'nollie big spin',
+'nollie bs shuv',
+'nollie fs shuv',
+'nollie',
+'nollie bs 180',
+'nollie fs 180',
+'fakie heelflip',
+'fakie ollie',
+'fakie big spin',
+
+];
+
+btn.addEventListener('click', function(){
+    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    output.innerHTML = randomQuote;
+})
